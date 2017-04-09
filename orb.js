8ed98ff7c040e8f2d654974efe84a82b939a7218
rortms <rortms@gmail.com>
2017-04-09 19:00:57 -0400

    feat: Add activate drop behavior
    When a drop is clicked within its center and half its radius it is
    "activated" and turns solid white. The remaining area of the drop
    is left to induce the drag behavior of the entire orb.

--- orb.js
+++ orb.js
@@ -11,12 +11,15 @@ function Orb(num_drops, color) {
     this.center = createVector(width/2, height/2);                // Default Orb center
     this.color = color;
 
-    // Initialize drops
-    this.drop_pos = [];
+    // Initialize drops    
+    this.drop_pos = [];     // position array
+    this.drop_clicked= [];  // 
+    
     for (var i = 0; i <=num_drops; i++) {
 	
-    	this.drop_pos.push(createVector(this.orb_rad * cos(TWO_PI / num_drops * i),
-    					this.orb_rad * sin(TWO_PI / num_drops * i)).add(this.center));
+	this.drop_pos.push(createVector(this.orb_rad * cos(TWO_PI / num_drops * i) ,
+					this.orb_rad * sin(TWO_PI / num_drops * i)).add(this.center));
+	this.drop_clicked[i] = false;
     }
 
     // Movement
@@ -42,19 +45,27 @@ function Orb(num_drops, color) {
 
 	var vary_D = this.drop_D + this.drop_D * 0.08 * random(-1,1);
 	
-	for (let pos of this.drop_pos)  {
+	//for (let pos of this.drop_pos)  {
+	for (var i = 0; i <=num_drops; i++) {
+	    pos = this.drop_pos[i]
 	    noStroke();
-	    fill(this.color);
-	    ellipse(pos.x, pos.y, vary_D, vary_D);
+	    if ( ! this.drop_clicked[i] ){
+		fill(this.color);
+		ellipse(pos.x, pos.y, vary_D, vary_D);
+	    } else {
+		fill(colors.white);
+		ellipse(pos.x, pos.y, this.drop_D * 0.5, this.drop_D * 0.5);
+	    }
 	}
 	///console.log("Big D: " + this.orb_D);
     }
 
     /// For grabbing the orb ///
     this.isHooked = function() {
-	var eps = 10
+	var eps = this.drop_D / 2 
 	var dist2_center = p5.Vector.dist(this.center,createVector(mouseX,mouseY));
-	if ( abs(dist2_center - this.orb_D / 2) < eps )
+	// Grab at edges but not at center of drop
+	if ( abs(dist2_center - this.orb_D / 2) < eps && abs(dist2_center - this.orb_D / 2) > eps / 2 )
 	    return true;
 	return false;
     }
@@ -64,6 +75,19 @@ function Orb(num_drops, color) {
 	if (this.hooked) {
 	    var translation = createVector(mouseX,mouseY).sub(this.drag_start);
 	    this.center = createVector(width/2,height/2).add(translation);
+	}
+    }
+
+    /// Set Drop to active
+    this.activateDrop = function (){
+	var eps = this.drop_D * 0.20;
+	var x = mouseX;
+	var y = mouseY;
+	for ( var i = 0; i < this.num_drops; i++) {
+	    if ( this.drop_pos[i].dist(createVector(x,y)) < eps) {
+		this.drop_clicked[i] = ! this.drop_clicked[i]
+		console.log(x,y)
+	    }
 	}
     }
     
--- sketch.js
+++ sketch.js
@@ -8,8 +8,8 @@ colors = { red : "#F44336",
 	   indigo: "#3F51B5",
 	   blue: "#2196F3",
 	   light_blue: "#03A9F4",
-	   cyan: "#00BCD4",
-	   ateal: "#1DE9B6",
+	   cyan: "#1DE9B6",
+	   green: "#76FF03",
 	   lime: "#C6FF00",
 	   yellow: "#FFEB3B",
 	   amber: "#FFC107",
@@ -24,29 +24,34 @@ console.log(colors.yellow);
 
 function setup() {
     createCanvas(800, 640);
-    flyer = new Flyer(colors.lime);
-    orb = new Orb(18, colors.blue);
+    flyer = new Flyer(colors.green);
+    orb = new Orb(20, colors.light_blue);
 }
 
 function draw() {
-    background(colors.black);    
+    background(colors.black);
     orb.displayOrb()
     flyer.move();
     flyer.displayFlyer();
 }
 
-// Make flyer track touch
+
 function mouseDragged() {
+    // Make flyer track touch
     flyer.trackTouch();
     console.log(orb.hooked);
     orb.drag()
 }
 
 function mouseClicked() {
-    if (orb.isHooked()) {	
+    // (double) click to hook orb
+    if (orb.isHooked()) {
 	orb.hooked = true;
 	orb.drag_start = createVector(mouseX,mouseY);
     }
+
+    // Activate drop
+    orb.activateDrop();
 }
 
 function mouseReleased() {
