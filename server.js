432434fae88cc375454d2f444be883316363a9a0
rortms <rortms@gmail.com>
2017-04-17 15:14:28 -0400

    refactor: Move files so that index.html is at root directory

--- server.js
+++
@@ -1,9 +0,0 @@
-var port = 8888
-var express = require('express');
-var app = express();
-var server = app.listen(port, function (){
-    console.log("Listening on port " + port);
-})
-
-app.use(express.static('ws_app'));
-    
